0348cdf6a96efdf48992a4a1e5856a470fde6a59
bacnk <92347442+bacnk@users.noreply.github.com>
2022-04-06 16:50:09 +0700

    https://github.com/bacnk/nodejs_test.git

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const hbs = require('express-handlebars');
+const { join } = require('path');
+const app = express();
+const port = 3000;
+const route = require('./routes');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+app.use(express.json());
+// http logger
+app.use(morgan('combined'))
+
+// template engine
+app.engine('handlebars', hbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+// console.log('PATH:', path.join(__dirname, 'resources/views'))
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+// app.get('/search', function (req, res) {
+
+//   res.render('search');
+// });
+
+app.post('/search', function (req, res) {
+  console.log(req.body);
+  res.send('');
+});
+// route init
+route(app);
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
